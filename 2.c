81ec78747bf49cf5fe18f732febe712994711e71
OlgaLyapina <olya.lyapina@mail.ru>
2021-07-29 21:28:22 +0300

    максимум из четырех

---
+++ 2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int a = 100;
+    int b = 200;
+
+    if (a > b){
+        printf("min = %d\n", b);
+    } else 
+    {
+        printf("min = %d\n", a);
+    }
+}
